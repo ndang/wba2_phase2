4dfcef6e5746bab26f30a2eb9d8f7d04e7a17acb
ndang <lenonoggi@web.de>
2013-06-19 15:29:38 +0200

    Main erstellt

---
+++ Phase2/src/GUI/InfoPopup.java
@@ -0,0 +1,84 @@
+package GUI;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.LayoutStyle.ComponentPlacement;
+
+public class InfoPopup extends JFrame
+{
+	private static String text;
+	
+	/**
+	 * Launch the application.
+	 */
+	public static void start(String meldung)
+	{
+		text = meldung;
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InfoPopup frame = new InfoPopup(text);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public InfoPopup(String meldung)
+	{
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 300, 150);
+		
+		JLabel lblMeldung = new JLabel(meldung);
+		
+		JButton btnOk = new JButton("OK");
+		btnOk.addActionListener(new ActionListener()
+		{	
+			@Override
+			public void actionPerformed(ActionEvent arg0)
+			{
+				close();
+			}
+		});
+		
+		GroupLayout groupLayout = new GroupLayout(getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.TRAILING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblMeldung)
+					.addContainerGap(234, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap(227, Short.MAX_VALUE)
+					.addComponent(btnOk)
+					.addContainerGap())
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblMeldung)
+					.addPreferredGap(ComponentPlacement.RELATED, 58, Short.MAX_VALUE)
+					.addComponent(btnOk)
+					.addContainerGap())
+		);
+		getContentPane().setLayout(groupLayout);
+	}
+	
+	private void close()
+	{
+		this.dispose();
+	}
+}
---
+++ Phase2/src/client/Main.java
@@ -0,0 +1,15 @@
+package client;
+
+import GUI.Login;
+
+public class Main {
+
+	/**Startet die Party-App mit einer LogIn-Aufforderung.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		Login.start();
+	}
+}
